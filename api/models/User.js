f32f37ea09c31a8fdece40e5bb8b80e3db4642ad
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2023-06-15 14:08:06 +0300

    Add Pages

--- api/models/User.js
+++ api/models/User.js
@@ -7,6 +7,6 @@ const UserSchema = new Schema({
     password: String
 })
 
-const UserModel = mongoose.model('User',UserSchema)
+const UserModel = mongoose.model('User', UserSchema)
 
 module.exports = UserModel
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,29 @@ import LoginPage from "./pages/LoginPage.jsx";
 import Layout from "./Layout";
 import RegisterPage from "./pages/RegisterPage";
 import axios from "axios";
+import {UserContextProvider} from "./UserContext";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import PlacesPage from "./pages/PlacePage.jsx";
+import PlacesFormPage from "./pages/PlacesFormPage";
 
-axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.baseURL = 'http://127.0.0.1:4000'
+axios.defaults.withCredentials = true
+
 function App() {
     return (
+        <UserContextProvider>
         <Routes>
             <Route path="/" element={<Layout/>}>
                 <Route index element={<IndexPage/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/account" element={<ProfilePage/>}/>
+                <Route path="/account/places" element={<PlacesPage/>}/>
+                <Route path="/account/places/new" element={<PlacesFormPage/>}/>
+                <Route path="/account/places/:id" element={<PlacesFormPage/>}/>
             </Route>
         </Routes>
-
+        </UserContextProvider>
     )
 }
 
